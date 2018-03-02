ea5e0e54ddff5b20fa9b5212072b9b481baaf044
Kanika Sharma <kanikash4@gmail.com>
2018-03-03 01:34:27 +0530

    interfaces additions

---
+++ hello-play/app/services/addData.java
@@ -0,0 +1,12 @@
+package services;
+
+import models.TokenResponse;
+import models.User;
+
+public interface addData {
+
+	// boolean userExists(String username);
+
+    String usrName(final String username);
+
+}
